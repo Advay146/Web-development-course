f319248d65f2da92a82b7c0c21c4357459f9a9a6
Advay146 <advaygupta146@gmail.com>
2025-01-22 21:18:02 +0530

    inital commit

---
+++ Intro to Html/Untitled-1.py
@@ -0,0 +1,10 @@
+print("How many dads do you have?")
+print("1. Two")
+print("2. Three")
+print("3. Four")
+print("Enter your answer:")
+chose = input()
+if chose == "Two":
+ print = ("Right answer")
+else :
+ print("Wrong answer")
